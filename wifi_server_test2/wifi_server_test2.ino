b56c6d920bccc8c0bed6c2c3005e628cdebf5711
Eric Gustafson <edg@cs.ucsb.edu>
2016-08-02 16:06:55 -0700

    Move to a ConnectionString for bootstrapping

--- wifi_server_test2/wifi_server_test2.ino
+++ wifi_server_test2/wifi_server_test2.ino
@@ -1,11 +1,11 @@
 /*
- Another silly test
+  Another silly test
 
- This one presents a configuration page, and saves the content to EEPROM.
- The format saved is exactly what the Azure IOTHUB sample code wants.
- The idea would be to load this if a reset button is held in.
- We could also do a few other things, like be able to clear the EEPROM.
- */
+  This one presents a configuration page, and saves the content to EEPROM.
+  The format saved is exactly what the Azure IOTHUB sample code wants.
+  The idea would be to load this if a reset button is held in.
+  We could also do a few other things, like be able to clear the EEPROM.
+*/
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
@@ -28,9 +28,7 @@ const char HTTP_ROOT[] = "<html>\
 <form action='/configure' method='POST'>Wifi Details:<br>\
   SSID:<input type='text' name='SSID' placeholder='MyWifiSSID'><br>\
   Password:<input type='password' name='PASSWORD' placeholder='password'><br>\
-  Azure Host:<input type='text' name='AZUREHOST' placeholder='Go get it from Azure!'><br>\
-  Azure Key:<input type='text' name='AZUREKEY' placeholder='Go get it from Azure!'><br>\
-  Azure EventHub ID:<input type='text' name='AZUREID' placeholder='Go get it from Azure!'><br>\
+  Azure Connection String:<input type='text' name='CONNSTR' placeholder='Go get it from Azure!'><br>\
   Location:<input type='text' name='GEO' placeholder='Go get it from Azure!'><br>\
   <input type='submit' name='SUBMIT' value='Save'></form>\
     <img src=\"/test.svg\" />\
@@ -46,13 +44,13 @@ ESP8266WebServer server ( 80 );
 const int led = 13;
 
 void handleRoot() {
-	digitalWrite ( led, 1 );
-	int sec = millis() / 1000;
-	int min = sec / 60;
-	int hr = min / 60;
+  digitalWrite ( led, 1 );
+  int sec = millis() / 1000;
+  int min = sec / 60;
+  int hr = min / 60;
 
-	server.send ( 200, "text/html", HTTP_ROOT );
-	digitalWrite ( led, 0 );
+  server.send ( 200, "text/html", HTTP_ROOT );
+  digitalWrite ( led, 0 );
 }
 
 
@@ -70,7 +68,7 @@ const char* id = "D1";
 const int BufferLen = 510;  // max eprom length, minus 2 for data length
 char buffer[BufferLen];
 int length;
-void saveSettingsToEEPROM(String ssid, String password, String host, String key, String id, String geo) {
+void saveSettingsToEEPROM(String ssid, String password, String cs, String geo) {
   // TODO FIXME: Somebody do some validation! :D
   Serial.println("Saving settings to eeprom...");
   EEPROM.begin(512);
@@ -92,7 +90,9 @@ void saveSettingsToEEPROM(String ssid, String password, String host, String key,
     _password.add(password);
   }
 
-
+  host = GetStringValue(splitStringByIndex(splitStringByIndex(cs, ';', 0), '=', 1));
+  id = GetStringValue(splitStringByIndex(splitStringByIndex(cs, ';', 1), '=', 1));
+  key = (char*)GetStringValue(splitStringByIndex(splitStringByIndex(cs, ';', 2), '=', 1));
   root["host"] = host;
   root["key"] = key;
   root["id"] = id;
@@ -110,22 +110,22 @@ void saveSettingsToEEPROM(String ssid, String password, String host, String key,
 }
 
 void handleNotFound() {
-	digitalWrite ( led, 1 );
-	String message = "File Not Found\n\n";
-	message += "URI: ";
-	message += server.uri();
-	message += "\nMethod: ";
-	message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
-	message += "\nArguments: ";
-	message += server.args();
-	message += "\n";
+  digitalWrite ( led, 1 );
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
 
-	for ( uint8_t i = 0; i < server.args(); i++ ) {
-		message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
-	}
+  for ( uint8_t i = 0; i < server.args(); i++ ) {
+    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+  }
 
-	server.send ( 404, "text/plain", message );
-	digitalWrite ( led, 0 );
+  server.send ( 404, "text/plain", message );
+  digitalWrite ( led, 0 );
 }
 
 void initWifi() {
@@ -143,7 +143,7 @@ void initWifi() {
   Serial.println ( ssid );
   Serial.print ( "IP address: " );
   Serial.println ( WiFi.localIP() );
-  
+
   // Setup AP
   WiFi.mode(WIFI_AP_STA);
 
@@ -159,28 +159,25 @@ void initWifi() {
   char AP_NameChar[AP_NameString.length() + 1];
   memset(AP_NameChar, 0, AP_NameString.length() + 1);
 
-  for (int i=0; i<AP_NameString.length(); i++)
+  for (int i = 0; i < AP_NameString.length(); i++)
     AP_NameChar[i] = AP_NameString.charAt(i);
 
   WiFi.softAP(AP_NameChar, WiFiAPPSK);
 }
 
-void doConfigure(){
+void doConfigure() {
   String msg;
-  if (server.hasArg("SSID") && 
-      server.hasArg("PASSWORD") && 
-      server.hasArg("AZUREHOST") &&
-      server.hasArg("AZUREKEY") &&
-      server.hasArg("AZUREID") &&
-      server.hasArg("GEO")){
+  if (server.hasArg("SSID") &&
+      server.hasArg("PASSWORD") &&
+      server.hasArg("CONNSTR")) {
     // TODO: Try a connection.
     // TODO: If it's good, save it to EEPROM
-    saveSettingsToEEPROM(server.arg("SSID"),server.arg("PASSWORD"),server.arg("AZUREHOST"),server.arg("AZUREKEY"),server.arg("AZUREID"),server.arg("GEO"));
+    saveSettingsToEEPROM(server.arg("SSID"), server.arg("PASSWORD"), server.arg("CONNSTR"), server.arg("GEO"));
     msg = "Settings saved!\n";
   }
-  else {  
-  msg = "Failed to save settings!";
-  Serial.println("Failed to save settings!");
+  else {
+    msg = "Failed to save settings!";
+    Serial.println("Failed to save settings!");
   }
   String content = "<html><body>";
   content += msg;
@@ -189,53 +186,53 @@ void doConfigure(){
 }
 
 void setup ( void ) {
-	pinMode ( led, OUTPUT );
-	digitalWrite ( led, 0 );
-	Serial.begin ( 115200 );
-	initWifi();
-	// EDG says: This is for automatically finding these things later, say, in an app
-	if ( MDNS.begin ( "scintilla" ) ) {
-		Serial.println ( "MDNS responder started" );
-	}
+  pinMode ( led, OUTPUT );
+  digitalWrite ( led, 0 );
+  Serial.begin ( 115200 );
+  initWifi();
+  // EDG says: This is for automatically finding these things later, say, in an app
+  if ( MDNS.begin ( "scintilla" ) ) {
+    Serial.println ( "MDNS responder started" );
+  }
 
-	server.on ( "/setup", handleRoot );
-	server.on ( "/test.svg", drawGraph );
-	server.on("/configure", doConfigure );
-	server.on("/", doStatus);
-	server.onNotFound ( handleNotFound );
-	server.begin();
-	Serial.println ( "HTTP server started" );
+  server.on ( "/setup", handleRoot );
+  server.on ( "/test.svg", drawGraph );
+  server.on("/configure", doConfigure );
+  server.on("/", doStatus);
+  server.onNotFound ( handleNotFound );
+  server.begin();
+  Serial.println ( "HTTP server started" );
 }
 
 void loop ( void ) {
-	server.handleClient();
+  server.handleClient();
 }
 
 void drawGraph() {
-	String out = "";
-	char temp[100];
-	out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
- 	out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
- 	out += "<g stroke=\"black\">\n";
- 	int y = rand() % 130;
- 	for (int x = 10; x < 390; x+= 10) {
- 		int y2 = rand() % 130;
- 		sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
- 		out += temp;
- 		y = y2;
- 	}
-	out += "</g>\n</svg>\n";
+  String out = "";
+  char temp[100];
+  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
+  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
+  out += "<g stroke=\"black\">\n";
+  int y = rand() % 130;
+  for (int x = 10; x < 390; x += 10) {
+    int y2 = rand() % 130;
+    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
+    out += temp;
+    y = y2;
+  }
+  out += "</g>\n</svg>\n";
 
-	server.send ( 200, "image/svg+xml", out);
+  server.send ( 200, "image/svg+xml", out);
 }
 
 
 void doStatus() {
-  
+
   String out = "<html><head><title>Scintilla Status</title>";
   out += "<style>body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }</style></head><body>";
   out += "<h1>Scintilla Configuration</h1>\n";
-  
+
   StaticJsonBuffer<1000> jsonBuffer;
   int address = 2;
   length = word(EEPROM.read(0), EEPROM.read(1));
@@ -250,7 +247,7 @@ void doStatus() {
   {
     Serial.println("parseObject() failed");
     out += "Current configuration invalid! Please set the configuration.\r\n";
-    
+
   }
   else {
     numWifi = root["wifi"];
@@ -258,7 +255,7 @@ void doStatus() {
     key = root["key"];
     id = root["id"];
     geo = root["geo"];
-  
+
     Serial.println(numWifi);
     for (size_t i = 0; i < numWifi; i++)
     {
@@ -268,12 +265,12 @@ void doStatus() {
       Serial.print(ssidi); Serial.print(" - ");
       out += "SSID: " + (String)ssidi + "<br>";
       out += "Password" + (String)"*********" + "<br>";
-      if (WiFi.status() != WL_CONNECTED) 
+      if (WiFi.status() != WL_CONNECTED)
         out += "Status: DISCONNECTED!!!\r\n";
       else {
         out += "Status: CONNECTED!!!\r\n";
         out += "IP Address: " + (String)WiFi.localIP() + "\r\n";
-        
+
       }
       Serial.println(pwdi);
     }
@@ -290,7 +287,7 @@ void doStatus() {
   out += "<a href=\"/setup\">Edit configuration</a>";
   out += "</body></html>";
   server.send ( 200, "text/html", out);
-  
+
 }
 
 
